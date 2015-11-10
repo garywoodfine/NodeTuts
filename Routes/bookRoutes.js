a86bd5473c0f283ce6a67f8bef88398e43a0b310
gary <gary.woodfine@threenine.co.uk>
2015-11-10 16:41:21 +0000

    Finished off verbs and added middleware to remove reptitive codes

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -22,17 +22,64 @@ var routes = function(Book){
             });
 
         });
-    bookRouter.route('/Books/:bookid')
-        .get(function (req,res){
-
-            Book.findById(req.params.bookid,function(err,book){
-                if(err)
+    bookRouter.use('/:bookid', function(req,res,next){
+            Book.findById(req.params.bookid,function(err,book) {
+                if (err)
                     res.status(500).send(err);
-                else
-                    res.json(book);
+                else if (book) {
+                    req.book = book;
+                    next();
+                }
+                else {
+                    res.status(404).send('no book found');
+                }
+            });
+    });
+    bookRouter.route('/:bookid')
+        .get(function (req,res){
+            res.json(req.book);
+
+        })
+        .put(function(req,res){
+                    req.book.author = req.body.author;
+                    req.book.title = req.body.title;
+                    req.book.genre = req.body.genre;
+                    req.book.read = req.body.read;
+            req.book.save(function(err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.book);
+                }
             });
 
-        });
+        })
+        .patch(function(req,res){
+            if(req.body._id)
+                 delete req.body._id;
+           for(var p in req.body)
+           {
+               req.book[p] = req.body[p];
+           }
+            req.book.save(function(err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.book);
+                }
+            });
+
+            })
+                .delete(function(req,res){
+                req.book.remove(function(err){
+                    if(err)
+                        res.status(500).send(err);
+                    else{
+                        res.status(204).send('Removed');
+                    }
+                });
+            });
+
     return bookRouter;
 };
 
