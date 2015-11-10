c228e4af26c8a2d1b0d61053f4e97d4a96586595
gary <gary.woodfine@threenine.co.uk>
2015-11-10 04:59:45 -0800

    A working sample to use for further exercise

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # NodeTuts
 Just a collection of NodeJS tutorial files, from a course I am currently doing.
 Nothing special here, just me learning and playing with new stuff for fun
+
+
+To Import Data into MongoDB for the demo use the bookJson.js
+ 'mongo bookAPI < booksJson.js'
+
+ Execute via the terminal and navigate to the Project directory
--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-/**
- * Created by gary on 11/9/15.
- */
-var express = require('express');
+
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/BookModel');
+var app = express();
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+    .get(function (req,res){
+        var query = req.query;
+       Book.find(query,function(err,books){
+           if(err)
+             res.status(500).send(err);
+           else
+             res.json(books);
+       });
+
+    });
+
+app.use('/api', bookRouter);
+
+
+app.get('/', function(req,res){
+    res.send('Welcome to my API');
+});
+
+app.listen(port, function(){
+    console.log('Gulp API Running on PORT: ' + port);
+});
